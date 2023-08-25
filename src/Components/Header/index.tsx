33ca50bb85bb8f470ed42e17ec688dc800420200
viet <vuquocviet0000@gmail.com>
2023-08-25 21:49:13 +0700

    commit

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,33 @@
+import "./Header.scss";
+import {ArrowDown, Stack} from "../../assets/SVG/SVGs";
+interface IProps{
+  userImgUrl:string,
+  slogan:string,
+  placeholderSearchBar:string,
+}
+function Header( headerProps: IProps){
+  return (
+    <div className="header">
+        <div className="browse">
+            <div className="browse-category">
+                Navigator
+                <ArrowDown/>
+            </div>
+            <div className="search-bar">
+                <label> <input type="text" placeholder={headerProps.placeholderSearchBar}/> </label>
+            </div>
+        </div>
+        <div className="header-title"><span>{ headerProps.slogan }</span></div>
+        <div className="profile">
+            <div className="user-profile">
+                <img src={headerProps.userImgUrl} alt="" className="user-img"/>
+            </div>
+            <div className="profile-menu">
+                <Stack/>
+                Menu
+            </div>
+        </div>
+    </div>
+  );
+}
+export default Header;
---
+++ src/assets/SVG/SVGs.tsx
@@ -0,0 +1,13 @@
+export function ArrowDown(){
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-chevron-down">
+      <path d="M6 9l6 6 6-6"/>
+    </svg>);
+}
+export function Stack(){
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-menu">
+      <path d="M3 12h18M3 6h18M3 18h18"/>
+    </svg>);
+}
+

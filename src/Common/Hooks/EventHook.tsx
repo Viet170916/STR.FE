1919da05d02df2a92f4efcf7e2f30db1c584156d
viet <vuquocviet0000@gmail.com>
2023-08-29 22:33:55 +0700

    commit

--- src/Common/Hooks/EventHook.tsx
+++ src/Common/Hooks/EventHook.tsx
@@ -1,21 +1,19 @@
 import { useEffect, useRef } from "react";
 
-export function useClickOutsideElement(outSideClickHandler:Function){
-    const listElement = useRef( null );
-    useEffect( () => {
+export function useClickOutsideElement( outSideClickHandler: Function ){
+  const listElement = useRef( null );
+  useEffect( () => {
       const clickOut =
         ( event: MouseEvent ) => {
           if( listElement.current && !listElement.current.contains( event.target as Node ) ){
-            outSideClickHandler(event);
+            outSideClickHandler( event );
           }
         };
-
-
-          document.addEventListener( "mousedown", clickOut );
-          return () => {
-              document.removeEventListener( 'mousedown', clickOut );
-          };
-      },
-      [] );
-    return listElement;
+      document.addEventListener( "mousedown", clickOut );
+      return () => {
+        document.removeEventListener( 'mousedown', clickOut );
+      };
+    },
+    [] );
+  return listElement;
 }

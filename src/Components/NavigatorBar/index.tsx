74356b34196bad5383e1aab209340ccfff9637dd
WeArePotatoess <shinigamitedomi@gmail.com>
2023-08-30 20:29:02 +0700

    update NavigatorBar

--- src/Components/NavigatorBar/index.tsx
+++ src/Components/NavigatorBar/index.tsx
@@ -11,9 +11,9 @@ function NavigatorBar(props: IProps): JSX.Element {
   return (
     <div className="navigator-bar">
       <Link to={"#"} className={selected==="home"?"selected":""}>Home</Link>
-      <Link to={"#"} className={selected==="home"?"selected":""}>IT Jobs</Link>
-      <Link to={"#"} className={selected==="home"?"selected":""}>IT Companies</Link>
-      <Link to={"#"} className={selected==="home"?"selected":""}>Upload CV</Link>
+      <Link to={"#"} className={selected==="jobs"?"selected":""}>IT Jobs</Link>
+      <Link to={"#"} className={selected==="companies"?"selected":""}>IT Companies</Link>
+      <Link to={"#"} className={selected==="uploadCV"?"selected":""}>Upload CV</Link>
       <Link to={"#"} className={selected==="home"?"selected":""}>Upload CV</Link>
       <Link to={"#"} className="selected">Upload CV</Link>
     </div>

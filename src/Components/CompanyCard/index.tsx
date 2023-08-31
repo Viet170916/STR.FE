8e549692b452cb4846bbe8b76cdf65f5518edf08
WeArePotatoess <shinigamitedomi@gmail.com>
2023-08-31 10:59:39 +0700

    update companyCard

--- src/Components/CompanyCard/index.tsx
+++ src/Components/CompanyCard/index.tsx
@@ -16,35 +16,42 @@ const CompanyCard: React.FC<IProps> = ({
   location,
   workingField,
   companyURL,
-  isFollowed
+  isFollowed,
 }) => {
   return (
-    <div className="card-container"> 
-      <Link to={"#"} className="single-company-card">
-        <div className="content-wrapper">
-          <img
-            src="https://images-na.ssl-images-amazon.com/images/I/71PL7BiZ5NL.jpg"
-            alt=""
-            className="company-card-img"
-          />
-          <div className="card-content">
-            <div className="company-name">
-              Stay And Fight
-              {companyName}
-            </div>
-            <div className="location">
-              Ha Noi
-              {location}
-            </div>
-            <div className="card-sum">
-              Readers of all ages and walks of life have drawn inspiration and
-              empowerment from Elizabeth Gilbert's books for years.
-            </div>
+    <Link to={"#"} className="single-company-card">
+      <div className="content-wrapper">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/71PL7BiZ5NL.jpg"
+          alt=""
+          className="company-card-img"
+        />
+        <div className="card-content">
+          <div className="company-name">
+            FPT Software
+            {companyName}
+          </div>
+          <div className="location">
+            Ha Noi
+            {location}
+          </div>
+          <div className="card-sum">
+            Readers of all ages and walks of life have drawn inspiration and
+            empowerment from Elizabeth Gilbert's books for years.
+          </div>
+          <div className="working-field">
+            Software
+            {workingField}
           </div>
         </div>
-      </Link>
-      <p className="follow-tag">{isFollowed?"Followed":"Follow"}</p>
-    </div>
+      </div>
+      <div className="follow-wrapper">
+        <p className="follow-tag">{isFollowed ? "Followed" : "Follow"}</p>
+        <p className="jobs-number">
+            4 jobs
+        </p>
+      </div>
+    </Link>
   );
 };
 
